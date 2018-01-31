2f0536ca688e5d9286a42924622d250272d939be
Juhyun Lim <wow5468@naver.com>
2018-01-31 17:28:55 +0900

    Added the color data sets saver.

--- stylelens/dataset/bluehack/color.py
+++ stylelens/dataset/bluehack/color.py
@@ -41,14 +41,24 @@ storage = s3.S3(AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY)
 class GoogleSaver:
     def __init__(self, directory):
         self.directory = directory
-        os.mkdir(os.path.join(directory, 'temp'))
+
+        if not os.path.exists(os.path.join(directory, 'temp')):
+            os.mkdir(os.path.join(directory, 'temp'))
 
     def __save_image_to_storage(self, crawled_item):
-        downloaded_file_name = self.__download_image(crawled_item['url'])
-        key = os.path.join(AWS_DIR, crawled_item['color_name'], downloaded_file_name + '.jpg')
-        is_public = True
-        file_url = storage.upload_file_to_bucket(AWS_DATASET_BUCKET, downloaded_file_name, key, is_public=is_public)
-        return file_url
+
+        try:
+            downloaded_file_name = self.__download_image(crawled_item['url'], crawled_item['product'])
+            if downloaded_file_name is not None:
+                key = os.path.join(os.path.join(AWS_DIR, crawled_item['color_name']), downloaded_file_name.split(sep='/')[-1])
+                file_url = storage.upload_file_to_bucket(AWS_DATASET_BUCKET, downloaded_file_name, key, is_public=True)
+                print(file_url)
+
+                return file_url
+        except Exception as ex:
+            print(ex)
+
+        return None
 
     def __crawl_from_google_images(self):
         crawled_data = []
@@ -56,24 +66,31 @@ class GoogleSaver:
             color_folder = os.path.join(os.path.join(self.directory, 'color'), color_item[0])
             if os.path.exists(color_folder):
                 for file in os.listdir(color_folder):
-                    with open(os.path.join(color_folder, file)) as f:
+                    if file == '.DS_Store':
+                        continue
+                    with open(os.path.join(color_folder, file), encoding='UTF-8') as f:
                         for line in f.readlines():
                             color_file_item = {
                                 'url': line.strip(),
+                                'product': file.split(sep='.')[0].lower(),
                                 'color_name': color_item[0],
                                 'color_code': color_item[1]
                             }
                             crawled_data.append(color_file_item)
         return crawled_data
 
-    def __download_image(self, url):
+    def __download_image(self, url, product):
         try:
-            # f = urllib.urlopen(urllib.quote(image_path.encode('utf8'), '/:'))
-            f = urllib.request.urlopen(url)
-            im = Image.open(f).convert('RGB')
-            im = im.resize((IMG_WIDTH, IMG_HEIGHT), Image.ANTIALIAS)
-            download_file_name = os.path.join(os.path.join(self.directory, 'temp'), str(uuid.uuid4()) + '.jpg')
-            im.save(download_file_name)
+            f = urllib.request.urlopen(url, timeout=30)
+            with Image.open(f) as im:
+                im.convert('RGB')
+                if im is None:
+                    print('%s is not image file.' % url)
+                else:
+                    im = im.resize((IMG_WIDTH, IMG_HEIGHT), Image.ANTIALIAS)
+                    download_file_name = os.path.join(os.path.join(self.directory, 'temp'),
+                                                      '%s_%s.jpg' % (product, str(uuid.uuid4())))
+                    im.save(download_file_name)
         except Exception as e:
             print(e)
 
@@ -85,14 +102,17 @@ class GoogleSaver:
         for crawled_item in self.__crawl_from_google_images():
             file_url = self.__save_image_to_storage(crawled_item)
 
+            if file_url is None:
+                continue
+
             color = {
                 'file': file_url,
                 'name': crawled_item['color_name'],
                 'code': crawled_item['color_code']
             }
 
-            api_instance.add_color(color)
-
+            api_response = api_instance.add_color(color)
+            print(color, api_response)
 
 if __name__ == '__main__':
     args = parser.parse_args()
